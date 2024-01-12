f50861c98576b19906654c2f780fa0185a2f4aaf
khe <slhgesa@naver.com>
2024-01-12 17:37:39 +0900

    24년 1월 12일자 수정 커밋 : KG이니시스 결제모듈 추가 , 교육신청 폼 6개 추가, 마이페이지 결제 내역 및 교육 신청 내역 조회,수정,취소신청 기능 추가...etc : 누락파일 재커밋

---
+++ src/main/java/com/mtf/edumarine/dto/EduApplyInfoDTO.java
@@ -0,0 +1,19 @@
+package com.mtf.edumarine.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EduApplyInfoDTO {
+    Integer rownum; //연번
+    String trainName;
+    String trainSeq;
+    String payStatus;
+    String seq;
+    String finalRegiDttm; //최종 변경 일시
+    String trainStartDttm;
+    String trainEndDttm;
+}
